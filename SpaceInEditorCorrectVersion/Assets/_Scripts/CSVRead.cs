e4256ea724d389348cbb0d8e23fe412f29210b7d
joseppi <josep.pi.serra@gmail.com>
2019-11-26 18:30:11 +0100

    CSVRead is ready for QA
    This means you can use it and everything SHOULD be fine. :smile:
    The data of the events is inside:

--- SpaceInEditorCorrectVersion/Assets/_Scripts/CSVRead.cs
+++ SpaceInEditorCorrectVersion/Assets/_Scripts/CSVRead.cs
@@ -3,36 +3,156 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CSVRead : MonoBehaviour
-{    
-    ArrayList arrPositionEvent;
+{
+    public PositionEventData[] arrPositionEvents;
+    public HitEvent[] arrHitEvents;
+    public SessionEventData[] arrSesionEvents;
+    public RoundEndEvent[] arrRoundEndEvents;
+    public ErrorEvent[] arrErrorEvents;
+
+    //We are using a static object to control the memory we use
+    static string[] lineData;
+
+    static PositionEventData positionDataReturn;
+    static HitEvent hitDataReturn;
+    static SessionEventData sessionDataReturn;
+    static ErrorEvent errorDataReturn;
+    static RoundEndEvent roundEndDataReturn;
     
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ReadPositionEvent();
+        ReadSessionEvent();
+        ReadHitEvent();
+        ReadRoundEndEvent();
+        ReadErrorEvent();
+
+        Debug.Log(arrErrorEvents.Length + " = ErrorEvents ");
+        Debug.Log(arrPositionEvents.Length + " = PositionEvents ");
+        Debug.Log(arrSesionEvents.Length + " = SessionEvents ");
+        Debug.Log(arrHitEvents.Length + " = HitEvents ");
+        Debug.Log(arrRoundEndEvents.Length + " = RoundEndEvents");       
+    }
+    void ReadPositionEvent()
+    {
         string positionPath = Application.dataPath + "/CSV/" + "PositionEvents.csv";
 
-        string fileData = System.IO.File.ReadAllText(positionPath.ToString());        
-        string[] lines = fileData.Split("\n"[0]);        
-        string[] lineData = (lines[1].Trim()).Split(","[0]);
-        PositionEventData positionEventData;
-        positionEventData.sessionID = int.Parse(lineData[0]);
-        positionEventData.round = int.Parse(lineData[1]);
-        positionEventData.timeStamp = float.Parse(lineData[2]);
-        positionEventData.posX = float.Parse(lineData[3]);
-        positionEventData.posY = float.Parse(lineData[4]);
-        positionEventData.posZ = float.Parse(lineData[5]);
-        positionEventData.rotX = float.Parse(lineData[6]);
-        positionEventData.rotY = float.Parse(lineData[7]);
-        positionEventData.rotZ = float.Parse(lineData[8]);
-        positionEventData.rotW = float.Parse(lineData[9]);
-        positionEventData.velocityX = float.Parse(lineData[10]);
-        positionEventData.velocityY = float.Parse(lineData[11]);
-        positionEventData.velocityZ = float.Parse(lineData[12]);
-        Debug.Log(positionEventData.velocityZ.ToString());
+        string fileData = System.IO.File.ReadAllText(positionPath.ToString());
+        string[] lines = fileData.Split("\n"[0]);
 
+        arrPositionEvents = new PositionEventData[lines.Length - 1]; //Unity "ReadAllText()" gets one extra row that has nothing, for some reason.
 
-
-
+        for (int i = 1; i < lines.Length - 1; i++) //i = 1 instead of 0 because we want to skip the headers
+        {
+            lineData = (lines[i].Trim()).Split(","[0]);
+            positionDataReturn.sessionID = int.Parse(lineData[0]);
+            positionDataReturn.round = int.Parse(lineData[1]);
+            positionDataReturn.timeStamp = float.Parse(lineData[2]);
+            positionDataReturn.posX = float.Parse(lineData[3]);
+            positionDataReturn.posY = float.Parse(lineData[4]);
+            positionDataReturn.posZ = float.Parse(lineData[5]);
+            positionDataReturn.rotX = float.Parse(lineData[6]);
+            positionDataReturn.rotY = float.Parse(lineData[7]);
+            positionDataReturn.rotZ = float.Parse(lineData[8]);
+            positionDataReturn.rotW = float.Parse(lineData[9]);
+            positionDataReturn.velocityX = float.Parse(lineData[10]);
+            positionDataReturn.velocityY = float.Parse(lineData[11]);
+            positionDataReturn.velocityZ = float.Parse(lineData[12]);
+            arrPositionEvents[i - 1] = positionDataReturn; //It's "i-1" because arrays starts with 0.             
+        }
     }
+    void ReadSessionEvent()
+    {
+        string positionPath = Application.dataPath + "/CSV/" + "SessionEvents.csv";
+
+        string fileData = System.IO.File.ReadAllText(positionPath.ToString());
+        string[] lines = fileData.Split("\n"[0]);
+
+        arrSesionEvents = new SessionEventData[lines.Length - 1]; //Unity "ReadAllText()" gets one extra row that has nothing, for some reason.
+        
+        for (int i = 1; i < lines.Length - 1; i++) //i = 1 instead of 0 because we want to skip the headers
+        {
+            lineData = (lines[i].Trim()).Split(","[0]);
+            sessionDataReturn.sessionID = int.Parse(lineData[0]);
+            sessionDataReturn.personID = lineData[1];
+            sessionDataReturn.timeStamp = int.Parse(lineData[2]);
+            sessionDataReturn.sessionEventType = int.Parse(lineData[3]);
+            arrSesionEvents[i - 1] = sessionDataReturn; //It's "i-1" because arrays starts with 0.             
+        }
+    }
+    void ReadHitEvent()
+    {
+        string positionPath = Application.dataPath + "/CSV/" + "HitEvents.csv";
+
+        string fileData = System.IO.File.ReadAllText(positionPath.ToString());
+        string[] lines = fileData.Split("\n"[0]);
+
+        arrHitEvents = new HitEvent[lines.Length - 1]; //Unity "ReadAllText()" gets one extra row that has nothing, for some reason.
+        
+        for (int i = 1; i < lines.Length - 1; i++) //i = 1 instead of 0 because we want to skip the headers
+        {
+            lineData = (lines[i].Trim()).Split(","[0]);
+            hitDataReturn.sessionID = int.Parse(lineData[0]);
+            hitDataReturn.timeStamp = float.Parse(lineData[1]);
+            hitDataReturn.ObstacleID = int.Parse(lineData[2]);
+            arrHitEvents[i - 1] = hitDataReturn; //It's "i-1" because arrays starts with 0.             
+        }
+    }
+    void ReadRoundEndEvent()
+    {
+        string positionPath = Application.dataPath + "/CSV/" + "RoundEndEvents.csv";
+
+        string fileData = System.IO.File.ReadAllText(positionPath.ToString());
+        string[] lines = fileData.Split("\n"[0]);
+
+        arrRoundEndEvents = new RoundEndEvent[lines.Length - 1]; //Unity "ReadAllText()" gets one extra row that has nothing, for some reason.
+        
+        for (int i = 1; i < lines.Length - 1; i++) //i = 1 instead of 0 because we want to skip the headers
+        {
+            lineData = (lines[i].Trim()).Split(","[0]);
+            roundEndDataReturn.sessionID = int.Parse(lineData[0]);
+            roundEndDataReturn.round = int.Parse(lineData[1]);
+            roundEndDataReturn.timeStamp = float.Parse(lineData[2]);
+            arrRoundEndEvents[i - 1] = roundEndDataReturn; //It's "i-1" because arrays starts with 0.             
+        }
+    }
+    void ReadErrorEvent()
+    {
+        string positionPath = Application.dataPath + "/CSV/" + "ErrorEvents.csv";
+
+        string fileData = System.IO.File.ReadAllText(positionPath.ToString());
+        string[] lines = fileData.Split("\n"[0]);
+
+        arrErrorEvents = new ErrorEvent[lines.Length - 1]; //Unity "ReadAllText()" gets one extra row that has nothing, for some reason.       
+
+        for (int i = 1; i < lines.Length - 1; i++) //i = 1 instead of 0 because we want to skip the headers
+        {
+            lineData = (lines[i].Trim()).Split(","[0]);
+            errorDataReturn.sessionID = int.Parse(lineData[0]);
+            errorDataReturn.timeStamp = float.Parse(lineData[1]);
+            errorDataReturn.type = GetErrorType(lineData[2]);
+
+            arrErrorEvents[i - 1] = errorDataReturn; //It's "i-1" because arrays starts with 0.     
+        }
+    }
+
+    ErrorType GetErrorType(string str)
+    {
+        ErrorType ret = ErrorType.None;
+        string errorType = str;
+        switch (errorType)
+        {
+            case "Stuck_in_Obstacle":
+                ret = ErrorType.Stuck_in_Obstacle;
+                break;
+
+            case "Fall_off_map":
+                ret = ErrorType.Fall_off_map;
+                break;
+        }
+        return ret;
+    }
+    
 }
