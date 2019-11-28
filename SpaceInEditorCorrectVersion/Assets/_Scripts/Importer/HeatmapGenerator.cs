6dd657887a013fa0e21cec71f6b968a57191a123
Skyway666 <lucasgarciamateu@gmail.com>
2019-11-28 19:58:31 +0100

    Heatmap generator uses gradiant

--- SpaceInEditorCorrectVersion/Assets/_Scripts/Importer/HeatmapGenerator.cs
+++ SpaceInEditorCorrectVersion/Assets/_Scripts/Importer/HeatmapGenerator.cs
@@ -8,12 +8,16 @@ public class HeatmapGenerator : MonoBehaviour
     public GameObject heatmapPlaneTemplate;
 
     bool heatmapGenerated = false;
-
+    // Variables to change in order to change heatmap
     Vector2 mapSize;
     int squareSize;
-
+    // Heatmap generation
     Vector2Int heatmapSize;
     int[,] heatmap;
+    // Colors
+    Gradient gradient;
+    GradientColorKey[] colorKey;
+    GradientAlphaKey[] alphaKey;
 
     // Start is called before the first frame update
     void Start()
@@ -32,7 +36,25 @@ public class HeatmapGenerator : MonoBehaviour
         for (int i = 0; i < heatmapSize.x; i++)
             for (int j = 0; j < heatmapSize.y; j++)
                 heatmap[i, j] = 0;
- 
+
+        gradient = new Gradient();
+
+        // Populate the color keys at the relative time 0 and 1 (0 and 100%)
+        colorKey = new GradientColorKey[2];
+        colorKey[0].color = Color.blue;
+        colorKey[0].time = 0.0f;
+        colorKey[1].color = Color.red;
+        colorKey[1].time = 1.0f;
+
+        // Populate the alpha  keys at relative time 0 and 1  (0 and 100%)
+        alphaKey = new GradientAlphaKey[2];
+        alphaKey[0].alpha = 1.0f;
+        alphaKey[0].time = 0.0f;
+        alphaKey[1].alpha = 1.0f;
+        alphaKey[1].time = 1.0f;
+
+        gradient.SetKeys(colorKey, alphaKey);
+
     }
 
     // Update is called once per frame
@@ -85,7 +107,7 @@ public class HeatmapGenerator : MonoBehaviour
                     // Set scale of squareSize: The plane is 10 unites long with a 1 scale, so in order to have 1:1 scale we need to multiply by 0.1
                     plane.transform.localScale = new Vector3(0.1f * squareSize, 1, 0.1f * squareSize); //
                     // Modify red color of plane depending on "eventsAmount"
-                    plane.GetComponent<MeshRenderer>().material.color = new Color(eventsAmount / doubleAvarage, 0, 0, 1);
+                    plane.GetComponent<MeshRenderer>().material.color = gradient.Evaluate(eventsAmount / doubleAvarage);
                 }
 
         }
